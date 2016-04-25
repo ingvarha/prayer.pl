6bf8055a0de1e0fa7f8f9788c2018529b328457b
Ingvar Hagelund <ingvar@redpill-linpro.com>
2016-04-25 13:22:13 +0200

    initial commit

---
+++ prayer.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+PRAYER:
+
+my $Lord = my $shepherd;
+my $wants = {};
+
+for ( ever, 
+let, me ) { sleep in,
+green, pastures
+and send me, beside, 
+still, waters 
+and my $soul = restored; }
+
+send me, 
+in, righteousness; 
+for ( your, names, sake ) {
+
+do stay if I;
+seek ( death and shadow 
+and I, shall, 
+fear ); not evil; 
+for ( $you = with, me, your ) 
+{ rod and $staff = my $comfort }
+
+for ( me, prepare, a ){table};
+in; the; presence;
+of; my $enemies and
+anoint; my $head; with oil
+until my $cup, overruns;
+
+Goodness and mercy; 
+shall follow me 
+until all, days
+and I; do last } 
+for (ever and ever, 
+in, Gods ) { house }
+
+# Psalm 23
+# A perl prayer
+# Ingvar Hagelund 2003
